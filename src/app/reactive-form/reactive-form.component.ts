e2d563b0fb32f30fbf13b8977a8795302ec57d5f
Viktoria Gonda <viktoria.gonda@gammaorg.hu>
2023-01-20 16:17:39 +0100

    add FormArray with Material Chips

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
   selector: 'app-reactive-form',
@@ -9,17 +10,42 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ReactiveFormComponent implements OnInit {
   genders = ['male', 'female'];
   signUpForm!: FormGroup;
+  hobbiesIndex!: number;
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
-      username: new FormControl(null, Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email]),
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
       gender: new FormControl('male'),
+      hobbies: new FormArray([
+        new FormControl('dancing'),
+        new FormControl('reading'),
+      ]),
     });
   }
 
   onSubmit() {
     console.log(this.signUpForm);
+  }
+
+  getControls() {
+    return (<FormArray>this.signUpForm.get('hobbies')).controls;
+  }
+
+  onAddHobby(event: MatChipInputEvent) {
+    const control = new FormControl(event.value, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+
+    event.chipInput!.clear();
+  }
+
+  removeHobby(hobby: string) {
+    const index = this.signUpForm.get('hobbies')?.value.indexOf(hobby);
+    if (index && index > -1) {
+      (<FormArray>this.signUpForm.get('hobbies')).removeAt(index);
+    }
   }
 
   // customValidator()
