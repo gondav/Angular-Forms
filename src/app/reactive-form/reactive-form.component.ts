31972249a9a193451bff038d44090f9c4d4c211a
Viktoria Gonda <viktoria.gonda@gammaorg.hu>
2023-01-20 13:51:35 +0100

    further updates on reactive form

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrls: ['./reactive-form.component.css'],
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit {
   genders = ['male', 'female'];
+  signUpForm!: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      gender: new FormControl('male'),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signUpForm);
+  }
+
+  // customValidator()
 }
+
+('Q: How to add custom validator to a FormControl?');
